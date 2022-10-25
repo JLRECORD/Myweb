9a79dadd5cff277d1f2d7ea51c23c0ea1d8a169e
JLRECORD <116110137+JLRECORD@users.noreply.github.com>
2022-10-25 18:11:10 -0400

    Add files via upload

---
+++ aaa.php
@@ -0,0 +1,12 @@
+ <?php
+  $name = $_GET['name'];
+  if ($name != ""){
+    echo "ay name";
+  }else{
+    echo "no ay name";
+  }
+ echo  "<h1>Gracias por participar</h1>";
+  echo "Tu nombre es ".$name;
+?>
+  <h1>Gracias por participar</h1>
+  
