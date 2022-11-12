5081aca86756c6646564774954afb9e147f21c59
JLRECORD <116110137+JLRECORD@users.noreply.github.com>
2022-11-12 10:16:43 -0500

    Add files via upload

---
+++ ArrayJson.js
@@ -0,0 +1,7 @@
+export var artistas = [
+  {nombre: 'Jorge',arte: 'Realizador audiovisual', referencia: 'Soy musico regaetonero y kthj hgjj jfhjj hfuuj hdjjj jfhuj hfhjj hghuj hfuujv hgcjfuk  jxuf hcy  ycy  jcy jcyccjcy ufyfvuuc y  yucy  ycu', img: 'img/img.png'},
+    {nombre: 'Dial',arte: 'Rapero', referencia: 'Soy musico regaetonero y kthj hgjj jfhjj h hdje jfkfuuduft ititf kfufif kfufufi jfjf7r8 jfud7e kfue6du jdys6d jfyd7f   fuuj hdjjj jfhuj hfhjj hghuj hfuujv hgcjfuk  jxuf hcy  ycy  jcy jcyccjcy ufyfvuuc y  yucy  ycu', img: 'img/p1.png'},
+      {nombre: 'Grego',arte: 'melodico', referencia: 'Soy musico regaetonero y kthj hgjj jfhjj hfuuj hdjjj jfhuj hfhjj hghuj hfuujv hgcjfuk  jxuf hcy  ycy  jcy jcyccjcy ufyfvuuc y  yucy  ycu', img: 'img/p2.png'},
+        {nombre: 'Gady Pagés',arte: 'musico', referencia: 'Soy musico regaetonero y kthj hgjj jfhjj hfuuj hdjjj jfhuj hfhjj hghuj hfuujv hgcjfuk  jxuf hcy  ycy  jcy jcyccjcy ufyfvuuc y  yucy  ycu', img: 'img/p3.png'},
+        {nombre: 'Chico Mega', arte: 'Repartero', referencia: 'El Chico Mega es un repartero de guane musico y bailarin, como los demas que me gusta la musica y todo mo que tiene q ver con las artes esenicas, Lo que estoy escrinbiendo aqui es mkerda ñorq es una prueba', img: 'img/p1.png'}
+  ];
