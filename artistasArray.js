0cedf9e8b3c23e4a7f992accf64f230196956ee6
JLRECORD <116110137+JLRECORD@users.noreply.github.com>
2025-07-03 21:16:37 -0400

    Delete artistasArray.js

--- artistasArray.js
+++
@@ -1,9 +0,0 @@
-export let artista = [
-  {name: "Gady Pagés", arte: "Regaetonero", perfil: "img/gadi.jpg", descripcion: "Gadi es cantante y mucico q le gusta el regae", wsap: "+5358168903", face: "https://www.facebook.com/jorgeluis.giniebra", insta: ""}, 
-  
-  {name: "Asiel Estrada", arte: "Rapero", perfil: "img/asiel.jpg", descripcion: "Asiel es cantante y mucico q le gusta el regae", wsap: "+5350185647", face: "", insta: ""}, 
-  
-  {name: "Yoslan", arte: "Regaetonero", perfil: "img/yoslan.jpg", descripcion: "Yoslan es cantante y mucico q le gusta el regae. también tiene años de experiencia en la escuela y la cantidad que se llama jorge de los datos necesarios para que usted tenga que realizar las gestiones pertinentes oportunas", wsap: "+5351377751", face: "", insta: ""}, 
-  
-  {name: "Gasolina", arte: "Rapero", perfil: "img/asiel.jpg", descripcion: "Asiel es cantante y mucico q le gusta el regae", wsap: "+5350185647", face: "", insta: ""}
-  ];
